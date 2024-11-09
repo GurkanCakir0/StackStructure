327339a88ce8ad4977d04229f4231bb0323ceb78
GURKAN-CAKIR <huseyingurkancakir01@gmail.com>
2024-11-09 23:04:36 +0300

    StackStructure

--- Stack/Stack/Form1.cs
+++ Stack/Stack/Form1.cs
@@ -17,9 +17,50 @@ namespace Stack
             InitializeComponent();
         }
 
+        private bool Uygunluk(char a, char k)
+        {
+            return (a == '(' && k == ')') || (a == '{' && k == '}') || (a == '[' && k == ']');
+        }
+
+        private bool Kontrol(string parantez)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char char1 in parantez)
+            {
+                if (char1=='(' || char1=='[' || char1=='{')
+                {
+                    stack.Push(char1);
+                }
+                else if (char1== ')' || char1==']' || char1=='}')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        char al = stack.Pop();
+                        if (!Uygunluk(al,char1))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            string parantez = textBox1.Text;
 
+            if (Kontrol(parantez))
+            {
+                MessageBox.Show("Parantezler Uyuşuyor");
+            }
+            else
+            {
+                MessageBox.Show("Parantezlerden Biri veya Bir Kaçı Eksik");
+            }
         }
     }
 }
